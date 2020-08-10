92ada1a6d1806766ef94e856ed57f5d97ea0031e
Josh <cesar.joshua.p.c@gmail.com>
2020-08-09 22:04:44 -0500

    Update BigInt.js

--- BigInt.js
+++ BigInt.js
@@ -2,3 +2,16 @@
 At the moment the maximum number you can store as an integer in JavaScript is pow(2, 53) - 1 . But BigInt actually allows you to go even beyond that.  
 */
 
+
+let shortInteger = Number.MAX_SAFE_INTEGER
+
+console.log(shortInteger)
+
+console.log(shortInteger++)
+
+let bigInteger = 9007199254740992n
+
+console.log(bigInteger)
+
+console.log(bigInteger++)
+
